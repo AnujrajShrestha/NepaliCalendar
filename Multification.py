15f3f4b01420e7fc83262f58f400f86311b52428
AnujrajShrestha <shrestharachana196@gmail.com>
2026-04-09 17:48:16 +0545

    calendar

--- Multification.py
+++
@@ -1,6 +0,0 @@
-def mutification_table(num):
-    for multiply in range(1,11,1):
-        print(f'{num} X {multiply} = {num*multiply}'.center(20))
-
-num=int(input("Enter the number "))
-mutification_table(num)
